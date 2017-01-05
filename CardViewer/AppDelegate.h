5583ea2605b9ad58d08a01c4c9e6b1d7a0e39f38
刘鹏 <lp_aideals@hotmail.com>
2017-01-05 16:25:28 +0800

    Initial Commit

---
+++ CardViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardViewer
+//
+//  Created by 鹏 刘 on 2017/1/5.
+//  Copyright © 2017年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardViewer
+//
+//  Created by 鹏 刘 on 2017/1/5.
+//  Copyright © 2017年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
